28a36c977172cf9447efd870856aeab6ccdbcf11
Jiang XiaoJun <2483690020@qq.com>
2018-10-30 22:23:20 +0800

    use xpath get items

--- douban-scrapy/douban/spiders/douban_spider.py
+++ douban-scrapy/douban/spiders/douban_spider.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from douban.items import DoubanItem
 
 class DoubanSpiderSpider(scrapy.Spider):
 	# 这里是爬虫名
@@ -11,4 +11,15 @@ class DoubanSpiderSpider(scrapy.Spider):
     start_urls = ['http://movie.douban.com/top250']
 
     def parse(self, response):
-        print(response.text)
+        # print(response.text)
+        #获取首页的25条电影信息
+        movie_list = response.xpath("//div[@class='article']//ol[@class='grid_view']//li")
+        for i_item in movie_list:
+            douban_item = DoubanItem()
+            douban_item['serial_number'] = i_item.xpath(".//div[@class='item']//em/text()").extract_first()
+            douban_item['movie_name'] = i_item.xpath(".//div[@class='info']/div[@class='hd']/a/span[1]/text()").extract_first()
+            content = i_item.xpath(".//div[@class='info']//div[@class='bd']/p[1]/text()").extract()
+            for i_content in content:
+                content_s = "".join(i_content.split())
+                douban_item['introduce'] = content_s
+            print(douban_item)
