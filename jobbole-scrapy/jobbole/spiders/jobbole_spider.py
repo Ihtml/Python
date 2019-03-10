3f2f59e9aae4c379e85c059c8a76abe35c1e50c1
Ihtml <2483690020@qq.com>
2019-03-10 22:49:28 +0800

    get next_url

--- jobbole-scrapy/jobbole/spiders/jobbole_spider.py
+++ jobbole-scrapy/jobbole/spiders/jobbole_spider.py
@@ -1,13 +1,34 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+import datetime 
+from scrapy.http import Request
+from urllib import parse
 
 
 class JobboleSpiderSpider(scrapy.Spider):
     name = 'jobbole_spider'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
+        """
+        1. 获取文章列表页中的文章url并交给scrapy下载后并进行解析
+        2. 获取下一页的url并交给scrapy进行下载， 下载完成后交给parse
+        """
+        #解析列表页中的所有文章url并交给scrapy下载后并进行解析
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            post_url = post_node.css("::attr(href)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url":image_url}, callback=self.parse_detail)
+
+        #提取下一页并交给scrapy进行下载
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    def parse_detail(self, response):
         # 提取文章的具体字段
         # title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
         # create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
