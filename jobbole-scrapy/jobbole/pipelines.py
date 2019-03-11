ef21fc3396901e745be4fcfb0f6ece816a1659a0
Ihtml <2483690020@qq.com>
2019-03-11 23:13:54 +0800

    自定义管道，拿到存放图片的本地路径

--- jobbole-scrapy/jobbole/pipelines.py
+++ jobbole-scrapy/jobbole/pipelines.py
@@ -5,7 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.pipelines.images import ImagesPipeline
 
 class JobbolePipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+# 自定义管道，拿到存放图片的本地路径
+class ArticleImagePipeline(ImagesPipeline):
+    def item_completed(self, results, item, info):
+        for ok, value in results:
+            image_file_path = value['path']
+        item['front_image_path'] = image_file_path
+
         return item
--- jobbole-scrapy/jobbole/settings.py
+++ jobbole-scrapy/jobbole/settings.py
@@ -66,12 +66,15 @@ ROBOTSTXT_OBEY = False
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'jobbole.pipelines.JobbolePipeline': 300,
-    'scrapy.pipelines.images.ImagesPipeline': 1,
+    # 'scrapy.pipelines.images.ImagesPipeline': 1,
+    'ArticleSpider.pipelines.ArticleImagePipeline': 1,  # 自定义图片的pipeline
 }
 IMAGES_URLS_FIELD = 'front_image_url'
 project_dir = os.path.abspath(os.path.dirname(__file__))
 IMAGES_STORE = os.path.join(project_dir, 'images')
-
+# 设置下载图片最小宽高
+IMAGES_MIN_HEIGHT = 100
+IMAGES_MIN_WIDTH = 100
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
