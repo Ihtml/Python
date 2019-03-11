c7ec5a10147625bc9c37526e06e0d774e8391160
Ihtml <2483690020@qq.com>
2019-03-11 22:33:15 +0800

    design items  save image

--- jobbole-scrapy/jobbole/spiders/jobbole_spider.py
+++ jobbole-scrapy/jobbole/spiders/jobbole_spider.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
-import datetime 
+import datetime
 from scrapy.http import Request
 from urllib import parse
+from jobbole.items import JobboleItem
 
 
 class JobboleSpiderSpider(scrapy.Spider):
@@ -29,6 +30,8 @@ class JobboleSpiderSpider(scrapy.Spider):
             yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
 
     def parse_detail(self, response):
+        article_item = JobboleItem()
+
         # 提取文章的具体字段
         # title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
         # create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
@@ -48,28 +51,41 @@ class JobboleSpiderSpider(scrapy.Spider):
         # tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
         # tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
         # tags = ",".join(tag_list)
-        
-        #通过css选择器提取字段
-        # front_image_url = response.meta.get("front_image_url", "")  #文章封面图
-        # title = response.css(".entry-header h1::text").extract()[0]
-        # create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
-        # praise_nums = response.css(".vote-post-up h10::text").extract()[0]
-        # fav_nums = response.css(".bookmark-btn::text").extract()[0]
-        # match_re = re.match(".*?(\d+).*", fav_nums)
-        # if match_re:
-        #     fav_nums = int(match_re.group(1))
-        # else:
-        #     fav_nums = 0
-        #
-        # comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
-        # match_re = re.match(".*?(\d+).*", comment_nums)
-        # if match_re:
-        #     comment_nums = int(match_re.group(1))
-        # else:
-        #     comment_nums = 0
-        #
-        # content = response.css("div.entry").extract()[0]
-        #
-        # tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
-        # tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
-        # tags = ",".join(tag_list)
+
+        # 通过css选择器提取字段
+        front_image_url = response.meta.get("front_image_url", "")  #文章封面图
+        title = response.css(".entry-header h1::text").extract()[0]
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
+
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
+
+        content = response.css("div.entry").extract()[0]
+
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
+
+        article_item['title'] = title
+        article_item['url'] = response.url
+        article_item['create_date'] = create_date
+        article_item['front_image_url'] = [front_image_url]
+        article_item['praise_nums'] = praise_nums
+        article_item['comment_nums'] = comment_nums
+        article_item['fav_nums'] = fav_nums
+        article_item['tags'] = tags
+        article_item['content'] = content
+        print(article_item)
+
+        yield article_item
