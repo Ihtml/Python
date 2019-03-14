b1d0df1851f1df0415667e4537b0b9cf74be9621
Ihtml <2483690020@qq.com>
2019-03-14 23:16:42 +0800

    item 预处理

--- jobbole-scrapy/jobbole/items.py
+++ jobbole-scrapy/jobbole/items.py
@@ -6,18 +6,41 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+
+
+def add_jobbole(value):
+    return value+"-bobby"
+
+
+def get_nums(value):
+    match_re = re.match(".*?(\d+).*", value)
+    if match_re:
+        nums = int(match_re.group(1))
+    else:
+        nums = 0
+
+    return nums
 
 
 class JobboleItem(scrapy.Item):
     # define the fields for your item here like:
-    title = scrapy.Field()
+    title = scrapy.Field(
+        input_processor = MapCompose(add_jobbole())
+    )
     create_date = scrapy.Field()
     url = scrapy.Field()
     url_object_id = scrapy.Field()
     front_image_url = scrapy.Field()
     front_image_path = scrapy.Field()
-    praise_nums = scrapy.Field()
-    comment_nums = scrapy.Field()
-    fav_nums = scrapy.Field()
+    praise_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    comment_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    fav_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
     tags = scrapy.Field()
     content = scrapy.Field()
