886760bb22301552e8fc492309c1f4c34c245e0c
Ihtml <2483690020@qq.com>
2019-03-29 23:21:18 +0800

    parse answer

--- zhihu-spider/zhihu/spiders/zhihu_spider.py
+++ zhihu-spider/zhihu/spiders/zhihu_spider.py
@@ -2,6 +2,7 @@
 import scrapy
 import time
 import re
+import json
 
 try:
     import urlparse as parse
@@ -206,44 +207,70 @@ class ZhihuSpiderSpider(scrapy.Spider):
 #         cookie_dict[cookie["name"]] = cookie["value"]
 #     return [scrapy.Request(url=self.start_urls[0], dont_filter=True, cookies=cookie_dict)]
 
-def parse(self, response):
-    """
-        提取出html页面中的所有url 并跟踪这些url进行一步爬取
-        如果提取的url中格式为 /question/xxx 就下载之后直接进入解析函数
-    """
-    all_urls = response.css("a::attr(href)").extract()
-    all_urls = [parse.urljoin(response.url, url) for url in all_urls]
-    all_urls = filter(lambda x: True if x.startswith("https") else False, all_urls)
-    for url in all_urls:
-        match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", url)
-        if match_obj:
-            # 如果提取到question相关的页面则下载后交由提取函数进行提取
-            request_url = match_obj.group(1)
-            yield scrapy.Request(request_url, headers=self.headers, callback=self.parse_question)
-        else:
-            # 如果不是question页面则直接进一步跟踪
-            yield scrapy.Request(url, headers=self.headers, callback=self.parse)
+    def parse(self, response):
+        """
+            提取出html页面中的所有url 并跟踪这些url进行一步爬取
+            如果提取的url中格式为 /question/xxx 就下载之后直接进入解析函数
+        """
+        all_urls = response.css("a::attr(href)").extract()
+        all_urls = [parse.urljoin(response.url, url) for url in all_urls]
+        all_urls = filter(lambda x: True if x.startswith("https") else False, all_urls)
+        for url in all_urls:
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", url)
+            if match_obj:
+                # 如果提取到question相关的页面则下载后交由提取函数进行提取
+                request_url = match_obj.group(1)
+                yield scrapy.Request(request_url, headers=self.headers, callback=self.parse_question)
+            else:
+                # 如果不是question页面则直接进一步跟踪
+                yield scrapy.Request(url, headers=self.headers, callback=self.parse)
 
 
-def parse_question(self, response):
-    # 处理question页面， 从页面中提取出具体的question item
-    if "QuestionHeader-title" in response.text:
-        match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", response.url)
-        if match_obj:
-            question_id = int(match_obj.group(2))
+    def parse_question(self, response):
+        # 处理question页面， 从页面中提取出具体的question item
+        if "QuestionHeader-title" in response.text:
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", response.url)
+            if match_obj:
+                question_id = int(match_obj.group(2))
 
-        item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
-        item_loader.add_css("title", "h1.QuestionHeader-title::text")
-        item_loader.add_css("content", ".QuestionHeader-detail")
-        item_loader.add_value("url", response.url)
-        item_loader.add_value("zhihu_id", question_id)
-        item_loader.add_css("answer_num", ".List-headerText span::text")
-        item_loader.add_css("comments_num", ".QuestionHeader-actions button::text")
-        item_loader.add_css("watch_user_num", ".NumberBoard-value::text")
-        item_loader.add_css("topics", ".QuestionHeader-topics .Popover div::text")
+            item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
+            item_loader.add_css("title", "h1.QuestionHeader-title::text")
+            item_loader.add_css("content", ".QuestionHeader-detail")
+            item_loader.add_value("url", response.url)
+            item_loader.add_value("zhihu_id", question_id)
+            item_loader.add_css("answer_num", ".List-headerText span::text")
+            item_loader.add_css("comments_num", ".QuestionHeader-actions button::text")
+            item_loader.add_css("watch_user_num", ".NumberBoard-value::text")
+            item_loader.add_css("topics", ".QuestionHeader-topics .Popover div::text")
 
-        question_item = item_loader.load_item()
+            question_item = item_loader.load_item()
 
-    yield scrapy.Request(self.start_answer_url.format(question_id, 20, 0), headers=self.headers,
-                         callback=self.parse_answer)
-    yield question_item
+        yield scrapy.Request(self.start_answer_url.format(question_id, 20, 0), headers=self.headers,
+                             callback=self.parse_answer)
+        yield question_item
+
+
+    def parse_answer(self, reponse):
+        # 处理question的answer
+        ans_json = json.loads(reponse.text)
+        is_end = ans_json["paging"]["is_end"]
+        next_url = ans_json["paging"]["next"]
+
+        # 提取answer的具体字段
+        for answer in ans_json["data"]:
+            answer_item = ZhihuAnswerItem()
+            answer_item["zhihu_id"] = answer["id"]
+            answer_item["url"] = answer["url"]
+            answer_item["question_id"] = answer["question"]["id"]
+            answer_item["author_id"] = answer["author"]["id"] if "id" in answer["author"] else None
+            answer_item["content"] = answer["content"] if "content" in answer else None
+            answer_item["parise_num"] = answer["voteup_count"]
+            answer_item["comments_num"] = answer["comment_count"]
+            answer_item["create_time"] = answer["created_time"]
+            answer_item["update_time"] = answer["updated_time"]
+            answer_item["crawl_time"] = datetime.datetime.now()
+
+            yield answer_item
+
+        if not is_end:
+            yield scrapy.Request(next_url, headers=self.headers, callback=self.parse_answer)
